267d799f0e694c162668c44f3901be745a85a77a
Pablo Rafael <pabloofael@gmail.com>
2025-03-18 11:08:17 -0300

    feat: Adicionar componente Link do next ao componente Product.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,6 @@ import { HomeContainer, Product } from "../styles/pages/home";
 import {useKeenSlider} from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 
-
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
 import { stripe } from "../lib/stripe";
 import type { GetStaticProps } from "next";
 import type Stripe from "stripe";
@@ -34,7 +30,11 @@ export default function Home({ products }: HomeProps) {
       {
         products.map((product) => {
           return (
-            <Product key={product.id} className="keen-slider__slide">
+            <Product 
+              key={product.id} 
+              href={`/product/${product.id}`} 
+              className="keen-slider__slide"
+            >
 
             <Image src={product.imageUrl} width={520} height={480} alt=""/>
             <footer>
