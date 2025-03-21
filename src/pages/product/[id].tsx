1865a59ee85bc1c71c073686dedece9acf596081
Pablo Rafael <pabloofael@gmail.com>
2025-03-21 12:22:53 -0300

    feat: Adicionar o axios para fazer requisições á rota checkout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,6 +4,9 @@ import type { GetStaticPaths, GetStaticProps } from "next"
 import { stripe } from "../../lib/stripe"
 import type Stripe from "stripe"
 import Image from "next/image"
+import axios from "axios"
+import { useState } from "react"
+import Head from "next/head"
 
 interface ProductProps {
   product: {
@@ -11,30 +14,59 @@ interface ProductProps {
     name: string;
     imageUrl: string;
     price: string;
-    description: string
+    description: string;
+    defaultPriceId: string;
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl  
+    } catch (err) {
+      //Conectar  com uma ferramenta de observabilidade (Datalog / Sentry)
+      setIsCreatingCheckoutSession(false)
+
+      alert('Falha ao redirecionar ao checkout!')
+    }
+  }
+
   return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={520} height={480} alt=""/>
-      </ImageContainer>
+    <>
+      <Head>
+        <title>
+          {product.name} | Ignite Shop
+        </title>
+      </Head>
+    
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt=""/>
+        </ImageContainer>
 
-      <ProductDetails>  
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
+        <ProductDetails>  
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
 
-        <p>
-          {product.description}
-        </p> 
+          <p>
+            {product.description}
+          </p> 
 
-        <button>
-          Comprar agora
-        </button>
-      </ProductDetails>
-    </ProductContainer>
+          <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   )
 }
 
@@ -67,6 +99,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL'
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
       revalidate: 60 * 60 * 1 //1 hour 
   }
