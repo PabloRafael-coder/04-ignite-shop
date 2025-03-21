a0f4f22496344397fa393d05b4569fe14ea51fa9
Pablo Rafael <pabloofael@gmail.com>
2025-03-21 12:15:55 -0300

    feat: Adicionar o componente Head para trabalhar com SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe";
 import type { GetStaticProps } from "next";
 import type Stripe from "stripe";
+import Head from "next/head";
 
 interface HomeProps {
   products: {
@@ -26,28 +27,36 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {
-        products.map((product) => {
-          return (
-            <Product 
-              key={product.id} 
-              href={`/product/${product.id}`} 
-              className="keen-slider__slide"
-              prefetch={false}
-            >
+    <>
+      <Head>
+        <title>
+          Home | Ignite Shop
+        </title>
+      </Head>
 
-            <Image src={product.imageUrl} width={520} height={480} alt=""/>
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+      {
+      products.map((product) => {
+        return (
+          <Product 
+            key={product.id} 
+            href={`/product/${product.id}`} 
+            className="keen-slider__slide"
+            prefetch={false}
+          >
+
+          <Image src={product.imageUrl} width={520} height={480} alt=""/>
+          <footer>
+            <strong>{product.name}</strong>
+            <span>{product.price}</span>
+          </footer>
+        </Product>
           )
         })
       }
-    </HomeContainer>
-  );
+      </HomeContainer>
+    </>
+    );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
