f23beef9acced686fef6a20c6bfa1bd5d00746d6
Pablo Rafael <pabloofael@gmail.com>
2025-05-09 12:48:54 -0300

    feat: Adicionar componente Link no logo do header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import type { AppProps } from "next/app";
-import { globalStyles } from "../styles/global";
-import logoImg from '../assets/logo.svg'
 import Image from "next/image";
+import Link from "next/link";
+
+import logoImg from '../assets/logo.svg'
+
 import { Container, Header } from "../styles/pages/app";
 
+import { globalStyles } from "../styles/global";
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        <Link href="/">
+          <Image src={logoImg} alt="" />
+        </Link>
       </Header>
 
       <Component {...pageProps} />
